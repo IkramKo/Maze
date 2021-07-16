ca35e8c31c612218def3c8f0155a356e0b4ec457
RamiKoh <kohil.ikram@hotmail.com>
2021-07-15 21:10:50 -0400

    Another attempt

---
+++ Tkinter_Maze.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from os import system
+system("cls")
+
+# Maze Window
+window = Tk()
+window.title("Maze") # Apparently could've also done window = Tk(className = 'Window Title')
+window.geometry("500x500")
+window.mainloop()
