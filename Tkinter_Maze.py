24de6c46c707b723f7e9b46263bbe57952afe829
RamiKoh <kohil.ikram@hotmail.com>
2021-07-23 13:59:37 -0400

    sleep

--- Tkinter_Maze.py
+++ Tkinter_Maze.py
@@ -3,6 +3,36 @@ from os import system
 from random import randint
 system("cls")
 
+
+"""
+    1. Init all cells to walls
+    2. Implement Prim's algo to generate navigable cells
+    3. Draw maze
+
+    Drawing should be the last step; if I draw the entire maze in black (walls) from the start, i cant draw over those cells with white
+    and to modify the color of an existing canvas object, i need its id/var name. Simpler to create the maze in the cell_map first and 
+    draw last.
+"""
+
+# Init maze with walls
+cell_size = 15
+maze_size = 50 # height = width
+
+# Apparently this is the best way to init a 2D array according to GfG
+cell_map = [['wall' for row in range(maze_size)] for column in range(maze_size)] # rows x columns, all walls at the beginning
+    
+
+# Implementation of Prim's Algorithm
+    # Pick random cell as starting point; make sure to start randomization at one, since 0 will be reserved for exits
+starting_row = randint(1, maze_size)
+starting_column = randint(1, maze_size)
+cell_map[starting_row][starting_column] = 'path'
+
+# Init array that will keep track of neighboring walls
+neighbors = []
+
+
+
 # Single cell drawing (at first i used create_rectangle directly in the code but its cleaner to have a separate function)
 def draw_cell(row, column, color):
     x1 = column * cell_size
@@ -10,29 +40,16 @@ def draw_cell(row, column, color):
     y1 = row * cell_size
     y2 = y1 + cell_size
     maze.create_rectangle(x1, y1, x2, y2, fill = color)
-    
-# Create square maze filled with walls
-cell_size = 15
-maze_size = 50 # height = width
-t = [ [0]*3 for i in range(3)]
-# Apparently this is the best way to init a 2D array according to GfG
-cell_map = [['wall' for row in range(maze_size)] for column in range(maze_size)] # rows x columns, all walls at the beginning
 
-def maze_init():
+def draw_maze():
     for row in range(maze_size):
         for column in range(maze_size):
-            cell_map[row][column] = 'wall'
-            draw_cell(row, column, 'black') # Walls are black, the path is white.
+            if cell_map[row][column] == 'wall':
+                draw_cell(row, column, 'black')
+            elif cell_map[row][column] ==  'path':
+                draw_cell(row, column, 'white')
 
-# Implementation of Prim's Algorithm
-    # Pick random cell as starting point; make sure to start randomization at one, since 0 will be reserved for exits
-starting_row = randint(1, maze_size)
-starting_column = randint(1, maze_size)
-
-cell_map[starting_row][starting_column] = 'path'
-#draw_cell(starting_row, starting_column, 'white')
-
-
+print(cell_map)
 # Maze Window
 window = Tk()
 window.title("Maze") # Apparently could've also done window = Tk(className = 'Window Title')
@@ -41,5 +58,19 @@ window.geometry("750x750")  # maze_size * cell_size = 15 * 50 = 750
 maze = Canvas(window, bg = 'grey', height = maze_size * cell_size, width = maze_size * maze_size)
 maze.pack()
 
-maze_init()
+draw_maze()
 window.mainloop()
+
+
+
+
+
+"""
+Hall of shame (deleted functions)
+
+def maze_init():
+    for row in range(maze_size):
+        for column in range(maze_size):
+            cell_map[row][column] = 'wall'
+            draw_cell(row, column, 'black') # Walls are black, the path is white.
+"""
