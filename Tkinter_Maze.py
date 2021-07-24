1fd5cc1074cad8a910ec8d070221fae243438e9e
RamiKoh <kohil.ikram@hotmail.com>
2021-07-24 16:00:54 -0400

    well, shit

--- Tkinter_Maze.py
+++ Tkinter_Maze.py
@@ -13,7 +13,7 @@ system("cls")
     and to modify the color of an existing canvas object, i need its id/var name. Simpler to create the maze in the cell_map first and 
     draw last.
 """
-
+"""
 # Init maze with walls
 cell_size = 15
 maze_size = 50 # height = width
@@ -22,14 +22,37 @@ maze_size = 50 # height = width
 cell_map = [['wall' for row in range(maze_size)] for column in range(maze_size)] # rows x columns, all walls at the beginning
     
 
+"""
+#            N3
+#    N4  CURR_CELL   N2
+#            N1
+"""
+# Verify and add neighboring wall cells that arent on the edge
+def check_neighbors(row, column, neighbors):
+    if row == 1 and column == 1:
+        # Append([0, 1]) adds [0, 1] as a row, Extend([0, 1]) adds 0, 1 to the array (stays 1D)
+        if cell_map[row + 1, column] == 'wall'
+            neighbors.append([row + 1, column]) # N1
+        neighbors.append([row, column + 1]) # N2
+    elif row == 1 and column != 1:
+        neighbors.append([row + 1, column]) # N1
+        neighbors.append([row, column + 1]) # N2
+        neighbors.append([row, column - 1]) # N4
+    elif row != 1 and column == 1:
+        neighbors.append([row + 1, column]) # N1
+        neighbors.append([row, column + 1]) # N2
+        neighbors.append([row - 1, column]) # N3
+    elif row != 1 and column != 1:
+        neighbors.append([row + 1, column]) # N1
+        neighbors.append([row, column + 1]) # N2
+        neighbors.append([row - 1, column]) # N3
+        neighbors.append([row, column - 1]) # N4
+
 # Implementation of Prim's Algorithm
     # Pick random cell as starting point; make sure to start randomization at one, since 0 will be reserved for exits
 starting_row = randint(1, maze_size)
 starting_column = randint(1, maze_size)
 cell_map[starting_row][starting_column] = 'path'
-
-# Init array that will keep track of neighboring walls
-neighbors = []
 
 
 
@@ -49,7 +72,15 @@ def draw_maze():
             elif cell_map[row][column] ==  'path':
                 draw_cell(row, column, 'white')
 
-print(cell_map)
+#print(cell_map)
+# Init array that will keep track of neighboring walls
+neighbors = []
+check_neighbors(starting_row, starting_column, neighbors)
+while
+
+print(neighbors)
+
+
 # Maze Window
 window = Tk()
 window.title("Maze") # Apparently could've also done window = Tk(className = 'Window Title')
@@ -60,7 +91,15 @@ maze.pack()
 
 draw_maze()
 window.mainloop()
+"""
 
+walls = []
+walls.append([0, 1])
+walls.append([1, 1])
+walls.append([2, 1])
+walls.append([3, 1])
+print(len(walls))
+print(len(walls[0]))
 
 
 
